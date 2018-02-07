7cc129ee15cb280fff0866cdac04f80147082e39
Ivan Ilin <vanilin356@gmail.com>
2018-02-07 22:34:55 +0300

    homework5 finished

--- java1/src/com/alien/java1/lesson5/Employee.java
+++ java1/src/com/alien/java1/lesson5/Employee.java
@@ -1,12 +1,21 @@
 package com.alien.java1.lesson5;
 
+import java.util.Random;
+
 public class Employee {
+    private static Random rnd = new Random();
+
     private String name;
     private String position;
     private String email;
     private String phone;
     private int salary;
     private int age;
+
+    String[] names = {"Василий", "Петр", "Иван", "Александр", "Кирилл", "Дарья", "Мария", "Александра", "Василиса", "Юлия"};
+    String[] positions = {"Программист", "Инженер", "Архитектор", "Бухгалтер", "Юрист", "Исследователь", "Уборщик", "Водитель", "Грузчик", "Директор"};
+    String[] emails = {"aaa@gmail.com", "bbb@gmail.com", "ccc@gmail.com", "ddd@gmail.com", "eee@gmail.com", "fff@gmail.com", "ggg@gmail.com", "hhh@gmail.com", "jjj@gmail.com", "kkk@gmail.com"};
+    String[] phones = {"000", "111", "222", "333", "444", "555", "666", "777", "888", "999"};
 
     public Employee(String name, String position, String email, String phone, int salary, int age){
         this.name = name;
@@ -15,5 +24,24 @@ public class Employee {
         this.phone = phone;
         this.salary = salary;
         this.age = age;
+    }
+
+    public Employee(){
+        this.name = names[rnd.nextInt(10)];
+        this.position = positions[rnd.nextInt(10)];
+        this.email= emails[rnd.nextInt(10)];
+        this.phone = phones[rnd.nextInt(10)];
+        this.salary = rnd.nextInt(100000000);
+        this.age = rnd.nextInt(100);
+    }
+
+    public void printEmployee(){
+        System.out.println("Сотрудник: " + name + " (" + position + "), возраст: " + age);
+        System.out.println("Зарплата: " + salary + "р / мес");
+        System.out.println("Контакты:" + phone + ", " + email);
+    }
+
+    public int getAge() {
+        return age;
     }
 }
--- java1/src/com/alien/java1/lesson5/HomeWork5.java
+++ java1/src/com/alien/java1/lesson5/HomeWork5.java
@@ -7,7 +7,55 @@ public class HomeWork5 {
     private static Scanner scanner = new Scanner(System.in);
     private static Random rnd = new Random();
 
+    private static String inputName, inputPosition, inputEmail, inputPhone;
+    private static int inputAge, inputSalary;
+
     public static void main(String[] args) {
-        System.out.println("Массив сотрудников");
+        System.out.println("Работа с классом сотрудник");
+        System.out.print("Введите размер массива: ");
+        int empArraySize = scanner.nextInt();
+        Employee[] employees = new Employee[empArraySize];
+        System.out.println("Введите 1 чтобы заполнить массив вручную");
+        System.out.println("Введите 2 чтобы сгенерировать массив автоматически");
+        switch (scanner.nextInt()){
+            case 1:
+                for (int i = 0; i < employees.length; i++) {
+                    System.out.println("Сотрудник №" + (i + 1));
+                    System.out.print("Введите имя: ");
+                    inputName = scanner.next();
+                    System.out.print("Введите должность: ");
+                    inputPosition = scanner.next();
+                    System.out.print("Введите email: ");
+                    inputEmail = scanner.next();
+                    System.out.print("Введите телефон: ");
+                    inputPhone = scanner.next();
+                    System.out.print("Введите зарплату: ");
+                    inputSalary = scanner.nextInt();
+                    System.out.print("Введите возраст: ");
+                    inputAge = scanner.nextInt();
+
+                    employees[i] = new Employee(inputName, inputPosition, inputEmail, inputPhone, inputSalary, inputAge);
+                }
+                break;
+            case 2:
+                for (int i = 0; i < employees.length; i++) {
+                    employees[i] = new Employee();
+                }
+                break;
+        }
+        System.out.println();
+        System.out.println(" ===== Список всех сотрудников ===== ");
+        for (int i = 0; i < employees.length; i++) {
+            employees[i].printEmployee();
+            System.out.println();
+        }
+
+        System.out.println(" ===== Список всех сотрудников старше 40 ===== ");
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getAge() >= 40) {
+                employees[i].printEmployee();
+                System.out.println();
+            }
+        }
     }
 }
