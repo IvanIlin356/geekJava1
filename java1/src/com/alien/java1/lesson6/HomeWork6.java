9aa7094a4870f48a99851b325ec69e81af58d97a
Ivan Ilin <vanilin356@gmail.com>
2018-02-08 23:07:56 +0300

    stagt homework6

---
+++ java1/src/com/alien/java1/lesson6/HomeWork6.java
@@ -0,0 +1,10 @@
+package com.alien.java1.lesson6;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class HomeWork6 {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Random rnd = new Random();
+
+}
